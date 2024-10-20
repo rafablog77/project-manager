83e718af1b0bc42305b35a6e05c9d31e1dbfa90b
rafablog77 <143825964+rafablog77@users.noreply.github.com>
2024-10-20 19:18:19 -0300

    Replace .ui file with compiled version

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
-import sys
-from PyQt6 import QtWidgets, uic
+import os
+import platform
+import subprocess
+from sys import argv
+
+from PyQt6 import QtWidgets
+
+from mainwindow import Ui_MainWindow
+
+from PyQt6.QtWidgets import QComboBox, QPushButton, QListWidget
+
+
+def open_file(path: str) -> None:
+    if platform.system() == "Windows":
+        os.startfile(path)
+    elif platform.system() == "Darwin":
+        subprocess.Popen(["open", path])
+    else:
+        subprocess.Popen(["xdg-open", path])
+
 
 if __name__ == '__main__':
-    app = QtWidgets.QApplication(sys.argv)
+    class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+        def __init__(self, *args, obj=None, **kwargs):
+            super(MainWindow, self).__init__(*args, **kwargs)
+            self.setupUi(self)
 
-    window = uic.loadUi("mainwindow.ui")
+
+    app = QtWidgets.QApplication(argv)
+
+    window = MainWindow()
     window.show()
     app.exec()
